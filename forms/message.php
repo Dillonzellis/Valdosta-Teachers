3867c7de8bf4177c7dfc23157e0d1ac2b197a5d8
Dillon Ellis <dellis@fcul.com>
2022-06-08 09:29:12 -0400

    original message file

---
+++ forms/message.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+
+
+  <title>Valdosta Teachers' FCU | Forms Message</title>
+
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,700,900,' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400italic,400' rel='stylesheet' type='text/css'>
+
+
+  <link rel="stylesheet" href="../lib/font-awesome/font-awesome-4.3.0.min.css">
+  <link rel="stylesheet" href="../css/flaticon.min.css">
+
+  <!-- Important Owl stylesheet -->
+  <link rel="stylesheet" href="../lib/owl-carousel/owl.carousel.css">
+  <link rel="stylesheet" href="../lib/owl-carousel/owl.theme.css">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/custom.css">
+
+  <link rel="stylesheet" href="../dist/remodal.css">
+  <link rel="stylesheet" href="../dist/remodal-default-theme.css">
+
+
+
+</head>
+<body class="sticky-header">
+<div id="page" class="site">
+
+  <header id="masthead" class="site-header">
+
+    <div id="header-placeholder"></div>
+
+    <div id="header-wrap">
+
+      <div id="actionbar">
+        <? $INC_DIR = $_SERVER["DOCUMENT_ROOT"]. "/"; include($INC_DIR. "topbar.html"); ?>
+      </div>
+
+
+      <nav class="navbar">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+
+            <? $INC_DIR = $_SERVER["DOCUMENT_ROOT"]. "/"; include($INC_DIR. "logo.html"); ?>
+
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-menu-wrap">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="secondary-navigation-wrap">
+            <ul id="secondary-menu" class="nav navbar-nav navbar-right">
+              <li class="hb"><? $INC_DIR = $_SERVER["DOCUMENT_ROOT"]. "/"; include($INC_DIR. "hb.html"); ?></li>
+
+            </ul>
+          </div><!-- /.navbar-collapse -->
+
+        </div><!-- /.container -->
+
+        <div id="primary-navigation-container" class="menu-background-right ">
+
+          <div class="container boxed-no-gutter">
+
+            <div class="background-menu"></div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="primary-menu-wrap">
+              <? $INC_DIR = $_SERVER["DOCUMENT_ROOT"]. "/"; include($INC_DIR. "nav.html"); ?>
+            </div><!-- .navbar-collapse -->
+          </div><!-- .container -->
+        </div> <!-- #primary-navigation-container -->
+
+      </nav>
+
+      <div class="title-bar">
+        <div class="container">
+          <h1>Forms</h1>
+        </div>
+      </div>
+
+    </div><!-- #header-wrap -->
+
+  </header><!-- #masthead -->
+
+  <div id="content" class="site-content">
+
+
+    <div id="breadcrumb">
+      <div class="container">
+        <ol class="breadcrumb">
+          <li><a href="/index.php" title="Home">Home</a></li>
+          <li class="active">Forms</li>
+        </ol>
+      </div>
+    </div>
+
+
+    <main id="main" class="site-main" >
+      <div class="container">
+
+        <p align="center"><STRONG><EM>CONSENT                     FOR ELECTRONIC DISCLOSURES UNDER THE ELECTRONIC SIGNATURES                     IN GLOBAL AND NATIONAL COMMERCE ACT</EM></STRONG></p>
+        <p><EM>Please                     read this information carefully and print a copy and/or retain                     this information electronically for future reference. </EM></p>
+        <p><STRONG>Introduction. </STRONG>You are submitting an application to Valdosta Teachers&rsquo;                      Federal Credit Union. In order to do this, we need you to                     consent to our giving you certain disclosures electronically.                     This document informs you of your rights when receiving legally                     required disclosures, notices and information ("Disclosures")                     from the Credit Union. By completing and submitting an application                     through us, you acknowledge receipt of this document and consent                     to the electronic delivery of such Disclosures. All agreements                     with the Credit Union are subject to the terms and conditions                     of your Credit Union Membership Agreement. </p>
+        <p><STRONG>Electronic                     Communications. </STRONG>Any Disclosures related to your Request                     will be provided to you electronically by clicking on the                     following links (<A href="/forms/hbpolicy.php"><STRONG><EM>Home Banking Policy</EM></STRONG></A>, <A href="/forms/hbpolicy.php"><STRONG><EM>Electronic Funds Transfer Agreement</EM></STRONG></A>).                    However, if you wish to obtain a paper copy of any of the                     Disclosures, you may write to us at: 102 E. Force Street, Valdosta,                     GA 31601 with the details of your request. Paper copies will                     be provided to you at no charge.</p>
+        <p><STRONG>Scope                     of Consent. </STRONG>You consent to receive Disclosures and                     to do business electronically. The following information and                     disclosures will be provided electronically: </p>
+        <UL>
+          <LI> the                       Agreements and Disclosures for all Services You elect to                       use; and, </LI>
+          <LI> all                       communications (including disclosures and notices) regarding                       online banking, all future amendments (including to the                       Agreements), and all notices of changes, suspension, restriction,                       and termination of e-Banking services. </LI>
+        </UL>
+        <p><STRONG>Hardware                     and Software Requirements.</STRONG> To access and retain the                     Disclosures electronically, you will need to use the following                     computer software and hardware: Internet Explorer 4.0 or above,                     Netscape Navigator 4.0 or above or equivalent software; and                     hardware capable of running this software.</p>
+        <p><STRONG>Withdrawing                     Consent. </STRONG>You understand that you may not withdraw                     consent as to the disclosures we will provide to you at this                     time with your application. You may withdraw your consent                     to do further business electronically with us at no cost to                     you. If you decide to withdraw your consent, the legal validity                     and enforceability of prior electronic Disclosures will not                     be affected.</p>
+        <p><STRONG>Changes                     to Your Contact Information.</STRONG> You should keep us informed                     of any change in your electronic or mailing address. You may                     write to Valdosta Teachers Federal Credit Union at: 102 E. Force Street,                     Valdosta, GA 31601 regarding any such changes.</p>
+        <p><STRONG>Electronic                     Records.</STRONG> To facilitate electronic commerce, to reduce                     the expense of records storage, and to obtain the benefits                     of faster access to records, you acknowledge and agree that                     we may in our discretion store all records electronically;                     and that we will not retain and have no obligation to retain                     any original documents for any period of time. This applies                     to all documentation including but not limited to checks,                     transaction records, notes, mortgages, deeds of trust and                     other loan and/or security documentation. You further acknowledge                     and understand that we will routinely destroy all original                     documentation. We may store records electronically via imaging,                     scanning, filming or other technology used in the financial                     services industry for the storage of documentation via internal                     processes or third-party processors that we approve for these                     services. You agree that such storage shall be secure, and                     further agree that such records shall for all purposes be                     recognized and admissible in evidence or otherwise to prove                     the agreements, rights and obligations of the parties pursuant                     to any such records. </p>
+        <p><STRONG>&ldquo;E-Mail&rdquo;                      and Facsimile Communications.</STRONG> You acknowledge and                     agree that the Internet is considered inherently insecure.                     Therefore, you agree that we have no liability to you whatsoever                     for any loss, claim or damages arising or in any way related                     to our response(s) to any e-mail or other electronic communication,                     which we in good faith believe you have submitted to us. We                     have no duty to investigate the validity or to verify any                     e-mail or other electronic communication; and may respond                     to an e-mail at either the address provided with the communication,                     the e-mail address in your Membership Application and Signature                     Card, or any other application or written communication actually                     received by us. Any account owner, co-borrower, or authorized                     user may change the e-mail address for statements or other                     information from us at any time.</p>
+        <p>Although                     having no obligation to do so, we reserve the right to require                     authentication of e-mails or electronic communications. The                     decision to require authentication is in the sole discretion                     of the Credit Union. We will have no obligation, liability                     or responsibility to you or any other person or company if                     we do not act upon or follow any instruction to us if a communication                     cannot be authenticated to our satisfaction. Further, the                     Credit Union may not immediately receive e-mail communications                     that you send. Also, we will not take action based on e-mail                     requests until we actually receive your message and have a                     reasonable opportunity to act. We reserve the right to require                     any notices from you be submitted to us in writing, and we                     may refuse to send certain information through unsecured e-mail                     communications. If you need to contact the Credit Union immediately                     regarding an unauthorized transaction, stop payment request,                     or otherwise, you may call the Credit Union at 229-247-9930.</p>
+        <p><EM><STRONG>You                     expressly consent and agree to us, our agents or any parties                     we authorize sending and your receiving any communications                     hereunder by facsimile or other electronic methods including                     any offers for Credit Union, affiliate or third party services                     and/or products. </STRONG></EM></p>
+        <p><STRONG>Links                     to Other Sites.</STRONG> Our website may contain links to                     third party websites. These links are provided solely as a                     convenience to you and not as an endorsement by the Credit                     Union of the contents on such third-party website. Credit                     Union is not responsible for the content of linked third-party                     sites and does not make any representations regarding the                     content or accuracy of materials on such third party websites.                     If you decide to access linked third-party websites, you do                     so at your own risk.</p>
+        <p><STRONG>YOUR                     ABILITY TO ACCESS DISCLOSURES.</STRONG> BY COMPLETING AND                     SUBMITTING YOUR REQUEST, YOU ACKNOWLEDGE THAT YOU CAN ACCESS                     THE ELECTRONIC DISCLOSURES IN THE DESIGNATED FORMATS DESCRIBED                     ABOVE.</p>
+        <p>&nbsp;</p>
+        
+        <p><a href="stoppay.php" style="color:#FFFFFF;" class="button-secondary pure-button">I Agree</a> I/we consent to electronic disclosure of all Electronic Records as indicated above, and confirm that I/we will receive and read the terms and conditions of all agreements, disclosures, notices and other documents provided electronically. If there is more than one Owner or Authorized User, etc., clicking the “I ACCEPT” you are consenting on behalf of all other co-owners and/or authorized signers to enroll the accounts you have selected in the Service, as applicable, and that you are authorized to consent on their behalf.</p>
+        
+        <p><a href="index.php" style="color:#FFFFFF;" onclick="goBack()" class="button-secondary pure-button">I Don't Agree</a> I/we do not consent to electronic disclosure and understand that this choice cancels the application or other transaction now being performed.</p>
+        <script>
+function goBack() {
+    window.history.back();
+}
+</script>
+        <div class="margin-30"></div>
+      </div>
+    <!-- /.container --></main>
+
+
+
+  </div> <!-- /#content -->
+
+  <footer id="footer" class="site-footer" >
+
+    <div id="footer-widget-container">
+     <? $INC_DIR = $_SERVER["DOCUMENT_ROOT"]. "/"; include($INC_DIR. "footer1.html"); ?> 
+    </div>
+
+    <div id="site-info">
+     <? $INC_DIR = $_SERVER["DOCUMENT_ROOT"]. "/"; include($INC_DIR. "footer2.html"); ?>
+    </div><!-- .site-info -->
+  </footer><!-- #footer -->
+</div> <!-- /#page -->
+
+
+
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins and Tructor JavaScript) -->
+<script src="../js/jquery-1.11.2.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="../js/bootstrap.min.js"></script>
+<script src="../lib/stellar/jquery.stellar.min.js"></script>
+
+<link rel="stylesheet" href="../css/animate.min.css">
+<script src="../js/wow.min.js"></script>
+
+
+
+<script src="../lib/owl-carousel/owl.carousel.min.js"></script>
+<script>
+  'use strict';
+
+  var carousel = $(".our-reviews");
+
+  carousel.owlCarousel({
+    navigation: false,
+    slideSpeed: 1000,
+    paginationSpeed: 1000,
+    singleItem: true,
+    autoPlay: 8000
+  });
+</script>
+
+<script src="../js/scripts.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../libs/jquery/dist/jquery.min.js"><\/script>')</script>
+<script src="../dist/remodal.js"></script>
+
+<!-- Events -->
+<script>
+  $(document).on('opening', '.remodal', function () {
+    console.log('opening');
+  });
+
+  $(document).on('opened', '.remodal', function () {
+    console.log('opened');
+  });
+
+  $(document).on('closing', '.remodal', function (e) {
+    console.log('closing' + (e.reason ? ', reason: ' + e.reason : ''));
+  });
+
+  $(document).on('closed', '.remodal', function (e) {
+    console.log('closed' + (e.reason ? ', reason: ' + e.reason : ''));
+  });
+
+  $(document).on('confirmation', '.remodal', function () {
+    console.log('confirmation');
+  });
+
+  $(document).on('cancellation', '.remodal', function () {
+    console.log('cancellation');
+  });
+
+//  Usage:
+//  $(function() {
+//
+//    // In this case the initialization function returns the already created instance
+//    var inst = $('[data-remodal-id=modal]').remodal();
+//
+//    inst.open();
+//    inst.close();
+//    inst.getState();
+//    inst.destroy();
+//  });
+</script>
+
+</body>
+</html>
